996b3515c2fcb050cae3f064bc12d3919845ffeb
Adrian Szczerba <adix1551@gmail.com>
2021-01-28 10:32:44 +0100

    Sorted list of stations

--- data.py
+++ data.py
@@ -77,6 +77,7 @@ class getDataFromAPI(object):
             # usuwanie błędnej stacji z API
             if id == 734:
                 del placesList[len(placesList) - 1]
+        placesList.sort(key=lambda x: x.name)
         return placesList
 
     """
@@ -204,20 +205,20 @@ class getDataFromAPI(object):
         return stationProperties
 
 
-def testowanie():
-    o = getDataFromAPI()
-    lista = o.getStationData()
-    for i in lista:
-        print("Id =", i.id, "Nazwa =", i.name)
-        p = o.getStationProperties(i.id)
-        print("\t", "Indeks jakości powietrza:", p.indexMain)
-        print("\t\t", "SO2", p.indexSO2)
-        print("\t\t", "NO2", p.indexNO2)
-        print("\t\t", "PM10", p.indexPM10)
-        print("\t\t", "PM25", p.indexPM25)
-        print("\t\t", "CO", p.indexCO)
-        print("\t\t", "C6H6", p.indexC6H6)
-        print("\t\t", "O3", p.indexO3)
+# def testowanie():
+#     o = getDataFromAPI()
+#     lista = o.getStationData()
+#     for i in lista:
+#         print("Id =", i.id, "Nazwa =", i.name)
+#         p = o.getStationProperties(i.id)
+#         print("\t", "Indeks jakości powietrza:", p.indexMain)
+#         print("\t\t", "SO2", p.indexSO2)
+#         print("\t\t", "NO2", p.indexNO2)
+#         print("\t\t", "PM10", p.indexPM10)
+#         print("\t\t", "PM25", p.indexPM25)
+#         print("\t\t", "CO", p.indexCO)
+#         print("\t\t", "C6H6", p.indexC6H6)
+#         print("\t\t", "O3", p.indexO3)
 
 
-testowanie()
+#testowanie()
