a0b2b1ea0a18ca34c5c81eb3414f4d26c535d67c
11stussys <62251525+11stussys@users.noreply.github.com>
2021-01-29 00:34:13 +0100

    Update application.py

--- application.py
+++ application.py
@@ -1,43 +1,4 @@
 
-"""from data import getDataFromAPI
-import ipinfo
-import socket
-import requests
-
-class AppBackground:
-    def yourLocation():
-        try:
-            f = requests.request('GET', 'http://myip.dnsomatic.com')
-            ip = f.text
-            access_token = '0cd3132a1d7552'
-            handler = ipinfo.getHandler(access_token)
-            details = handler.getDetails(ip)
-            print(details.loc[0:7])
-            print(details.loc[8:])
-            return details.loc
-        except:
-            print("Serwer przeciążony")
-
-    def getStationLocation():
-        places_list = getDataFromAPI.getStationData()
-        station_lat=()
-        station_lon=()
-        for i in places_list:
-            station_lat +=  i.lat,
-            station_lon += i.lon,
-        return station_lat,station_lon
-
-
-def testowanie():
-    lista = AppBackground.getStationLocation()
-    for i in lista:
-        print(i.station_lat,i.station_lon)
-
-#def stationDistance():
-#yourLocation()
-#getStationLocation()
-"""
-
 from data import getDataFromAPI
 import ipinfo
 import socket
